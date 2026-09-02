52a2cff2a4d7e6f85ac1342419a207cccd8eafac
Fraser Masterson <fraser.masterson@yahoo.co.uk>
2026-09-02 15:38:02 +0100

    Update file paths and reset OUT file handling

--- 1_ Saving PCE annotations.R
+++ 1_ Saving PCE annotations.R
@@ -12,11 +12,11 @@ penguin = 'penguinID'
 # ID of specific video annotated
 vid = '1'
 
-# VANTAGE output (OUT) file
-out_file = read_csv(paste0('data/em_labelled_files/', penguin, '_25Hz_EM_classifications_OUT.csv', sep = ''))
+# VANTAGE output (OUT) file of individual
+out_file = read_csv(paste0('data/em_labelled_files/EM-classified accelerometry data_OUT.csv', sep = ''))
 
-# Original EM-labelled accelerometry file
-em_file = read_csv(paste0('data/em_labelled_files/', penguin, '_25Hz_EM_classifications.csv', sep = ''))
+# Original EM-labelled accelerometry file of individual
+em_file = read_csv(paste0('data/em_labelled_files/EM-classified accelerometry data.csv', sep = ''))
 
 # Adding original dataset's timestamp to the output data and filtering to only include annotated timestamps
 out_file$Timestamp = em_file$Timestamp
@@ -25,8 +25,8 @@ pce_events = filter(out_file, PCE > 0)
 # Saving the video's PCE annotations to a unique file
 write_csv(pce_events, file = paste('data/pce_files/', penguin, '_PCE_', vid, '.csv', sep = ''))
 
-# Resetting the OUT file for the next video
+# Resetting the individual's OUT file for the next video
 out_file_new = out_file
 out_file_new$PCE = 0
 out_file_new = out_file_new[-6]
-write_csv(out_file_new, file = paste0('data/em_labelled_files/', penguin, '_25Hz_EM_classifications_OUT.csv', sep = '')) 
+write_csv(out_file_new, file = paste0('data/em_labelled_files/EM-classified accelerometry data_OUT.csv', sep = '')) 
